01f9737cb71f5722c8a1818e8c7e8bed86c666cc
Elizenai Silva <elizenai.silva@hotmail.com>
2022-05-11 22:27:09 -0300

    upload the project in github

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-/*
-  Aquí va tu JavaScript
-*/
+function checar1() {
+
+  var elemento = document.getElementById("resposta1");
+    elemento.innerHTML = "A resposta correta é: 6 meses";
+  var radio = document.querySelector('input[name="resposta1"]:checked').value;
+    alert(radio);
+  }
+
+  function checar2() {
+
+  var elemento = document.getElementById("resposta2");
+  elemento.innerHTML = "A resposta correta é: JavaScript, HTML, CSS e frameworks";
+  var radio = document.querySelector('input[name="resposta2"]:checked').value;
+  alert(radio);
+}
